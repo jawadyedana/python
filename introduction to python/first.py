e67d181a00a1d9d2875bcc94d3dc32b7991caa32
jawadyedana <iamjawad754@gmail.com>
2025-06-26 10:47:39 +0000

    jawad

---
+++ introduction to python/first.py
@@ -0,0 +1,11 @@
+# console.log("Hello world") => in javascipt
+# print("Hello world")
+
+# var = value
+name = "Jawad" #string datatype
+age = 17 #number datatype
+skills = "HTML and CSS" #stringdatatype
+isTeacher = True #boolean
+
+print(f"My name is {name}, my ge is {age}, and my slills are {skills}")
+
---
+++ introduction to python/second.py
@@ -0,0 +1,13 @@
+#print a sentence 
+print("Welcome to the world of programming!")
+
+#print a Number
+print(4)
+
+#Next Line after print
+print("Hello python \n")
+
+#Print all keywords present in python
+print("Keywords in python are....")
+import keyword
+print(keyword.kwlist)
