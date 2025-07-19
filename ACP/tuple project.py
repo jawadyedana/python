25b5bbfb74a1ca6bbd9b08072de4a968856497b2
jawadyedana <iamjawad754@gmail.com>
2025-07-19 22:43:24 +0000

    project

---
+++ ACP/tuple project.py
@@ -0,0 +1,15 @@
+def tuple_to_list(tup):
+# Convert the tuple to a list
+    return list(tup)
+
+# Create a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Print the tuple
+print("Tuple:", my_tuple)
+
+# Convert the tuple to a list
+my_list = tuple_to_list(my_tuple)
+
+# Print the list
+print("List:",my_list)
