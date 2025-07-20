6a3af8bc6d3f9484f0150558b2a64ec97eb3dd6b
jawadyedana <iamjawad754@gmail.com>
2025-07-20 02:05:44 +0000

    ff

---
+++ ACP/act.js
@@ -0,0 +1,23 @@
+// Create an object
+let person = {
+    name: "John",
+    age: 30,
+    hobbies: ["Reading", "Swimming", "Coding"]
+};
+
+// Create an array
+let colors = ["Red", "Green", "Blue"];
+
+// Display values using object keys
+console.log("Person's name:", person.name);
+console.log("Person's age:", person.age);
+
+// Display values using array index
+console.log("First color:", colors[0]);
+
+// Display whole array using join
+console.log("Colors:", colors.join(", "));
+
+// Use pop function
+console.log("Popped color:", colors.pop());
+console.log("Colors after pop:", colors.join(", "));
